96be6ad616ab8c482d72050ae52f0d2c82a57f08
Matt Klich <matt.klich@readytalk.com>
2012-10-01 11:01:17 -0600

    Adding example local_settings.py file

---
+++ saic/local_settings.example.py
@@ -0,0 +1,19 @@
+LOCAL_SETTINGS = True
+from settings import *
+
+DEBUG = True
+
+ADMINS = (
+        ('Justin Bruce Van Horne', 'JUSTIN.B.VAN.HORNE@saic.com'),
+)
+
+TIME_ZONE = None
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '14qix7@zh_00qlyqw+$s(=e)^$7o8xfj(vpj0fws)y@7c1l^bc'
+
+# Debug toolbar
+#MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+#INSTALLED_APPS += ('debug_toolbar',)
+#INTERNAL_IPS = ('127.0.0.1')
+
